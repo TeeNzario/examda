064c7804832185c6a03cc182ccfa735375158395
TeeNzario <teen.2555@gmail.com>
2026-02-12 20:57:35 +0700

    Add api.ts for build

---
+++ client/services/api.ts
@@ -0,0 +1,39 @@
+import axios from "axios";
+import * as SecureStore from "expo-secure-store";
+
+const API_BASE_URL = "http://192.168.88.162:3001";
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Request interceptor to add auth token
+api.interceptors.request.use(
+  async (config) => {
+    const token = await SecureStore.getItemAsync("authToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+// Response interceptor for error handling
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response?.status === 401) {
+      // Token expired or invalid - will be handled by AuthContext
+      await SecureStore.deleteItemAsync("authToken");
+    }
+    return Promise.reject(error);
+  },
+);
+
+export default api;
